1e6ec1f528b0594a8f432d35ee92a16952d436c3
MdIsmailZabiullah <munshiismail262@gmail.com>
2023-03-23 11:49:56 +0530

    commit

--- client/src/Pages/Questions/DisplayAnswer.jsx
+++ client/src/Pages/Questions/DisplayAnswer.jsx
@@ -1,53 +1,63 @@
-import React from 'react'
-import { Link, useParams } from 'react-router-dom'
-import moment from 'moment'
-import {useSelector,useDispatch} from 'react-redux'
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { deleteAnswer } from "../../actions/question";
+import Avatar from "../../components/Avatar/Avatar";
+import moment from "moment";
+import "./QuestionDetails.css";
 
-import Avatar from '../../components/Avatar/Avatar'
-import './QuestionDetails.css'
-import {deleteAnswer} from '../../actions/question'
+const DisplayAnser = ({ question, handleShare }) => {
+  const { id } = useParams();
 
-const DisplayAnser = ({question, handleShare}) => {
-  
- const User = useSelector((state) => (state.currentUserReducer))
- const {id} = useParams()
- const dispatch= useDispatch()
- const handleDelete = (answerId,noOfAnswer)=> {
-  dispatch(deleteAnswer(id,answerId,noOfAnswer))
-}
+  const User = useSelector((state) => state.currentUserReducer);
+  const dispatch = useDispatch();
 
+  const handleDelete = (answerId, noOfAnswer) => { 
+        console.log(1)
+        dispatch(deleteAnswer(id, answerId, noOfAnswer));
+        
+  };
+
+  console.log(question)
   return (
     <div>
-      {
-        question.answer.map((ans) => (
-          <div className='display-ans' key={ans._id}>
-            <p>{ans.answerBody}</p>
-            <div className="question-action-user">
-              <div>
-                <button type='button' onClick={handleShare}>Share</button>
-                {
-                  User?.result?._id === ans?.userId &&(
-                    <button type='button' onClick={handleDelete}>Delete</button>
-                  )
-                }
-                
-              </div>
-              <div>
-                <p>Answered  {moment(ans.answeredOn).fromNow()}</p>
-                <Link to={`/user/${ans.userId}`} className='user-link' style={{color:'black'}}>
-                  <Avatar backgroundColor='green' px='5px' py='12px' >{ans.userAnswered.charAt(0).toLocaleUpperCase()}</Avatar>
-                  <div>
-                    {ans.userAnswered}
-                  </div>
-                </Link>
-              </div>
+      {question.answer.map((ans) => (
+        <div className="display-ans" key={ans._id}>
+          <p>{ans.answerBody}</p>
+          <div className="question-action-user">
+            <div>
+              <button type="button" onClick={handleShare}>
+                Share
+              </button>
+              {User?.result?._id === ans?.userId && (
+                <button
+                  type="button"
+                  onClick={() => {
+                    handleDelete(ans._id, question.noOfAnswers);
+                  }}
+                >
+                  Delete
+                </button>
+              )}
             </div>
-
+            <div>
+              <p>Answered {moment(ans.answeredOn).fromNow()}</p>
+              <Link
+                to={`/user/${ans.userId}`}
+                className="user-link"
+                style={{ color: "black" }}
+              >
+                <Avatar backgroundColor="green" px="5px" py="12px">
+                  {ans.userAnswered.charAt(0).toLocaleUpperCase()}
+                </Avatar>
+                <div>{ans.userAnswered}</div>
+              </Link>
+            </div>
           </div>
-        ))
-      }
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default DisplayAnser
+export default DisplayAnser;
--- client/src/components/Homemainbar/Question.jsx
+++ client/src/components/Homemainbar/Question.jsx
@@ -1,33 +1,68 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import moment from 'moment'
+// import React from 'react'
+// import {Link} from 'react-router-dom'
+// import moment from 'moment'
 
-const Question = ({question}) => {
+// const Question = ({question}) => {
+//   return (
+//     <div className='display-question-container'>
+//         <div className='display-vote-ans'>
+//             <p>{question.upVotes - question.downVotes}</p>
+//             <p>votes</p>
+//         </div>
+//         <div className='display-vote-ans'>
+//             <p>{question.noOfAnswers}</p>
+//             <p>Answer</p>
+//         </div>
+//         <div className='display-question'>
+//           <Link to={`/Questions/${question._id}`} className='question-link'>{question.questionTitle}</Link>
+//           <div className='display-tags-time'>
+//             <div className='display-tags'>
+//               {
+//                 question.questionTags.map((tag)=>(
+//                   <p key={tag}>{tag}</p>
+//                 ))
+//               }
+//             </div>
+//             <p className='display-time' >Asked  {moment(question.askedOn).fromNow()} by {question.userPosted}</p>
+//           </div>
+//         </div>      
+//     </div>
+//   )
+// }
+
+// export default Question
+import React from "react";
+import { Link } from "react-router-dom";
+import moment from "moment";
+
+const Question = ({ question }) => {
   return (
-    <div className='display-question-container'>
-        <div className='display-vote-ans'>
-            <p>{question.upVotes - question.downVotes}</p>
-            <p>votes</p>
-        </div>
-        <div className='display-vote-ans'>
-            <p>{question.noOfAnswers}</p>
-            <p>Answer</p>
-        </div>
-        <div className='display-question'>
-          <Link to={`/Questions/${question._id}`} className='question-link'>{question.questionTitle}</Link>
-          <div className='display-tags-time'>
-            <div className='display-tags'>
-              {
-                question.questionTags.map((tag)=>(
-                  <p key={tag}>{tag}</p>
-                ))
-              }
-            </div>
-            <p className='display-time' >Asked  {moment(question.askedOn).fromNow()} by {question.userPosted}</p>
+    <div className="display-question-container">
+      <div className="display-vote-ans">
+        <p>{question.upVote.length - question.downVote.length || 0}</p>
+        <p>votes</p>
+      </div>
+      <div className="display-vote-ans">
+        <p>{question.noOfAnswers}</p>
+        <p>Answer</p>
+      </div>
+      <div className="display-question">
+        <Link to={`/Questions/${question._id}`} className="question-link">
+          {question.questionTitle}
+        </Link>
+        <div className="display-tags-time">
+          <div className="display-tags">
+            {question.questionTags.map((tag) => (
+              <p key={tag}>{tag}</p>
+            ))}
           </div>
-        </div>      
+          <p className="display-time">
+            Asked {moment(question.askedOn).fromNow()} by {question.userPosted}
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Question
+export default Question;
